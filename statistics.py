28567767dad8e7e771c0369569264cd1b685657a
Lawrence (Chuck) Williams <lawrence.williams@modivcare.com>
2021-06-12 09:28:16 -0600

    update stats script

--- statistics.py
+++ statistics.py
@@ -5,25 +5,46 @@ import scipy.stats
 # ppf is the percent point function or inverse of cdf
 
 def z_score(x, mu, sigma):
+    '''
+    This function calculates the z-score, or how many 
+    standard deviations x lies from the mean of a
+    normal distribution
+    '''
     return (x-mu)/sigma
 
-def p_value(x, mu, sigma):
+def probability(x, mu, sigma):
+    '''
+    This function calculates the probability of a random sample
+    being less than or equal to x
+    '''
     return scipy.stats.norm(mu, sigma).cdf(x)
 
 def x_from_z(z_score, mu, sigma):
+    '''
+    This fucntion calculates the value in the normal distribution
+    from the z-score, or at a specified number of deviations from
+    the mean.
+    '''
     return z_score * sigma + mu
 
-def x_from_pval(pval, mu, sigma):
+def x_from_probability(pval, mu, sigma):
+    '''
+    This function calculates value of x from a specified 
+    probability, or the inverse of cdf. What value is at the 
+    percentile..
+    '''
     return scipy.stats.norm(mu, sigma).ppf(pval)
+
+
+def z_from_probability(pval, mu, sigma):
+    x = x_from_probability(pval, mu, sigma)
+    return z_score(x, mu, sigma)
 
 
 
 if __name__ == '__main__':
 
-    mu = 1100
-    sigma = 200
-    x = 1500 
-    print(z_score(x, mu, sigma))
-    print(p_value(x, mu, sigma))
-    print(x_from_z(z_score(x, mu, sigma), mu, sigma))
-    print(x_from_pval(p_value(x, mu, sigma),mu, sigma))
+    print(z_from_probability(.68, 1100, 200))
+    print(z_from_probability(.95, 1100, 200))
+    print(z_from_probability(.997, 1100, 200))
+  
